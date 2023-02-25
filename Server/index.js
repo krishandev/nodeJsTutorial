572d8ea3f436f6ac42426970d8166c23ea165438
Krishan <krishandev.0102@gmail.com>
2023-02-25 13:09:58 +0530

    change

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,5 @@
 const http=require("http");
+const fs=require("fs");
 const server= http.createServer((req, res)=>{
     if(req.url=="/"){
         res.end("This is my first app on Node Js")
@@ -7,7 +8,9 @@ const server= http.createServer((req, res)=>{
         res.end("this is download page");
     }
     else if(req.url==="/about"){
-        res.end("this is about page");
+        res.end(fs.readFileSync("api.json", "utf-8"));
+        
+        
     }
     else{
         res.end("404 page")
---
+++ Server/jsontest.js
@@ -0,0 +1,9 @@
+const obje={
+    name:"Krishan",
+    age:32,
+    address:"Noida"
+}
+
+const jsondata=JSON.stringify(obje)
+console.log(obje)
+console.log(jsondata)
