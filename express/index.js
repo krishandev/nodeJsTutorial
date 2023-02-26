f0d47ff56a10567e380a71401c83edffa63f16b9
Krishan <krishandev.0102@gmail.com>
2023-02-26 15:39:03 +0530

    change

--- express/index.js
+++ express/index.js
@@ -2,11 +2,16 @@ const express=require('express');
 const app=express();
 const path=require("path");
 
-const dirpath= path.join(__dirname, 'public')
-app.use(express.static(dirpath))
+// const dirpath= path.join(__dirname, 'public')
+// app.use(express.static(dirpath))
+
+app.set("view engine", "ejs");
+console.log(app.get("view engine"));
+console.log(path.join(__dirname), "views");
 
 app.get("/", (req, res)=>{
-    res.send(`<h1>This is my first express js app</h1>`)
+    // res.send(`<h1>This is my first express js app</h1>`)
+ res.render("index", {title:"this is index file"})
 })
 
 app.get("/about", (req, res)=>{
@@ -24,7 +29,8 @@ app.get("/about", (req, res)=>{
 })
 
 app.get("/download", (req, res)=>{
-    res.download(`${dirpath}/home.html`)
+    // res.download(`${dirpath}/home.html`)
+    res.render("home", {title:"This is home file"})
 })
 app.listen(3000, ()=>{
     console.log("Server is started at 3000 port")
