3de38904fcb4678167eb0b48b867180cfd147f62
Krishan <krishandev.0102@gmail.com>
2023-02-27 23:00:43 +0530

    change

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/nodejstutorial').then(()=>{
 })
 
 const schema=new mongoose.Schema({
-    name:String
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmPassword:{
+        type:String,
+        required:true
+    }
 })
 
 const user=mongoose.model("user", schema);
--- database/server.js
+++ database/server.js
@@ -1,65 +1,75 @@
-const express=require("express");
-const app=express();
-const user=require("./connection")
+const express = require("express");
+const app = express();
+const user = require("./connection")
 
 app.use(express.json());
 
 
-app.post("/", async(req, res)=>{
-try {
-    const senddata=new user(req.body)
-    const savedata=senddata.save();
-    res.send(savedata);
-} catch (error) {
-    res.status(404).send(error);
-}
+app.post("/", async (req, res) => {
+    try {
+        const senddata = new user(req.body)
+        const savedata = senddata.save();
+        res.send(savedata);
+    } catch (error) {
+        res.status(404).send(error);
+    }
 });
 
-app.get("/user", async(req, res)=>{
+app.get("/user", async (req, res) => {
     try {
-        const getdata=await user.find({});
+        const getdata = await user.find({});
         res.send(getdata);
-        
+
     } catch (error) {
         res.status(404).send(error);
     }
 
 });
 
-app.get("/user/:id", async(req, res)=>{
+app.get("/user/:id", async (req, res) => {
     try {
-        const id=req.params.id;
-        getdatabyid=await user.findById({_id:id});
+        const id = req.params.id;
+        getdatabyid = await user.findById({ _id: id });
         res.send(getdatabyid);
     } catch (error) {
         res.status(404).send(error)
     }
 });
 
-app.patch("/update/:id", async(req, res)=>{
+app.patch("/update/:id", async (req, res) => {
 
     try {
-        const id=req.params.id;
-        const updatedata=await user.findByIdAndUpdate({_id:id}, req.body, {new:true})
+        const id = req.params.id;
+        const updatedata = await user.findByIdAndUpdate({ _id: id }, req.body, { new: true })
         res.send(updatedata)
-        
+
     } catch (error) {
         res.status(500).send(error)
     }
 
 });
 
-app.delete("/delete/:id", async(req, res)=>{
+app.delete("/delete/:id", async (req, res) => {
     try {
-        const id=req.params.id;
-        const deletedata=await user.findByIdAndDelete({_id:id})
+        const id = req.params.id;
+        const deletedata = await user.findByIdAndDelete({ _id: id })
         res.status(500).send(deletedata)
-        
+
     } catch (error) {
         res.status(500).send(error)
     }
 
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        const useremail = req.body.email;
+        const getemail = await user.findOne({ email: useremail });
+        res.send(getemail);
+    } catch (error) {
+        res.status(404).send(error)
+    }
 })
-app.listen(3000, (req, res)=>{
+app.listen(3000, (req, res) => {
     console.log("Server Created Successfully")
 })
